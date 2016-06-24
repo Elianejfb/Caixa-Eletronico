5c6c7e314d7e0587b8c66ce92cacd3a62c61272a
Elianejfb <lilicaoj@yahoo.com.br>
2016-06-23 21:37:26 -0300

    Update Caix.java

--- Saque/src/Caix.java
+++ Saque/src/Caix.java
@@ -3,7 +3,7 @@ public class Caix {
 	int saque = 784;
     int cem = 0, cinquenta = 0, vinte = 0, dez = 0, dois = 0;
 	int valor; // guarda o valor lido
-	int temp=// variável auxiliar 
+	int temp=// variÃ¡vel auxiliar 
 
 		temp = saque
 		
@@ -12,10 +12,7 @@ public class Caix {
 	    cem = valor / 100;
 	    temp = valor % 100;
 	  
-	  // quantas notas de 50?
-	  if(temp >= 50){
-	    cinquenta = temp / 50;
-	    temp = temp % 50;
+	
 	  }
 	  // quantas notas de 20?
 	  if(temp >= 20){
